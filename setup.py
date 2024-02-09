ee0951a266d6c7c4f63345f3feae48710c6c1e56
tobiaskatch <tobis.katsch42@gmail.com>
2024-02-09 03:59:10 +0100

    readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gate-loop',
-    version='0.1.0',
+    version='1.0.0',
     author='Tobias Katsch',
     author_email='tobias.katsch42@gmail.com',
     packages=find_packages(),
