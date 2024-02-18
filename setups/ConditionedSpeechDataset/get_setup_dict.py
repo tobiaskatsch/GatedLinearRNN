cdecd7301cd95d2a61b1d338d841dff4fc67f86e
tobiaskatch <tobis.katsch42@gmail.com>
2024-02-18 02:06:57 +0100

    conditional speech

--- setups/ConditionedSpeechDataset/get_setup_dict.py
+++ setups/ConditionedSpeechDataset/get_setup_dict.py
@@ -80,8 +80,8 @@ def get_model_setup_dict(model_class_name, model_variation_name):
         speech_vocab_size=1024,
         text_vocab_size=72,
         max_seq_length=2000,
-        text_embedding_dropout=0.1,
-        speech_embedding_dropout=0.1,
+        text_embedding_dropout=0.,
+        speech_embedding_dropout=0.7,
     )
 
     module_name = f"setups.ConditionedSpeechDataset.{model_class_name}"
