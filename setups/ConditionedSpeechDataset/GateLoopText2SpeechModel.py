dc223692f6f9060d80f0206bc5f382007d632fca
tobiaskatch <tobis.katsch42@gmail.com>
2024-02-19 02:04:05 +0100

    conditional speech

--- setups/ConditionedSpeechDataset/GateLoopText2SpeechModel.py
+++ setups/ConditionedSpeechDataset/GateLoopText2SpeechModel.py
@@ -3,12 +3,12 @@ import jax.numpy as jnp
 def get_model_hparams(model_variation_name):
     if model_variation_name == "expressive":
         return dict(
-            d_h=384*2,
+            d_h=512*2,
             use_true_recurrence=True,
         )
     elif model_variation_name == "efficient":
         return dict(
-            d_h=384*2,
+            d_h=512*2,
             use_true_recurrence=False,
         )
     else:
