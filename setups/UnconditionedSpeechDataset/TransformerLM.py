4324afbeaabde0ddbda3fe45eb2e755b1189b799
tobiaskatch <tobis.katsch42@gmail.com>
2024-02-22 00:57:16 +0100

    renamed

--- setups/UnconditionedSpeechDataset/TransformerLM.py
+++ setups/UnconditionedSpeechDataset/TransformerLM.py
@@ -4,6 +4,7 @@ def get_model_hparams(model_variation_name):
     return dict(
         positional_encoding_mode="sinusoidal",
         n_head=6,
+        d_h=384 * 2,
         use_causal_mask=True,
     )
 
